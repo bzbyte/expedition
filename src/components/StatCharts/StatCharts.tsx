115971ff94785e9b4d268a2038a2619007687791
zk-qed <rahulksnv+1@gmail.com>
2023-06-26 04:16:08 +0000

    Chart changes

--- src/components/StatCharts/StatCharts.tsx
+++ src/components/StatCharts/StatCharts.tsx
@@ -4,7 +4,7 @@ import { hashesToGH } from "../formatters";
 import { hexToNumber } from "@etclabscore/eserialize";
 import { Grid } from "@material-ui/core";
 import ChartCard from "../ChartCard";
-import { VictoryLine, VictoryBar, VictoryChart } from "victory";
+import { VictoryLine, VictoryBar, VictoryChart, VictoryAxis } from "victory";
 import { useTranslation } from "react-i18next";
 
 const config = {
@@ -41,6 +41,26 @@ const blockMapTransactionCount = (block: any) => {
     y: block.transactions.length,
   };
 };
+const blockMapBtcPrice = (block: any) => {
+  return {
+    x: hexToNumber(block.number),
+    y: 30440.0 + Math.floor(Math. random() * (25 - 1 + 1)) + 1,
+  };
+};
+const BtcPrice = () => {
+  let ret = 30440.0 + Math.floor(Math. random() * (25 - 1 + 1)) + 1;
+  return " [ $" +  ret.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " ]";
+};
+const blockMapEthPrice = (block: any) => {
+  return {
+    x: hexToNumber(block.number),
+    y: 1890.0 + Math.floor(Math. random() * (10 - 1 + 1)) + 1,
+  };
+};
+const EthPrice = () => {
+  let ret = 1890.0 + Math.floor(Math. random() * (10 - 1 + 1)) + 1;
+  return " [ $" +  ret.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " ]";
+};
 
 interface IProps {
   blocks: any[];
@@ -76,6 +96,24 @@ const StatCharts: React.FC<IProps> = ({ blocks, victoryTheme }) => {
         <ChartCard title={t("Uncles")}>
           <VictoryChart height={config.chartHeight} width={config.chartWidth} theme={victoryTheme as any}>
             <VictoryBar data={blocks.map(blockMapUncles)} />
+          </VictoryChart>
+        </ChartCard>
+      </Grid>
+      <Grid key="BtcPrice" item xs={12} md={6} lg={3}>
+        <ChartCard title={t("BTC" + BtcPrice())}>
+          <VictoryChart height={config.chartHeight} width={config.chartWidth} theme={victoryTheme as any}>
+            <VictoryAxis crossAxis standalone={false} />
+            <VictoryAxis dependentAxis crossAxis domain={[30200, 30800]} standalone={false} />
+            <VictoryLine data={blocks.map(blockMapBtcPrice)} />
+          </VictoryChart>
+        </ChartCard>
+      </Grid>
+      <Grid key="ethPrice" item xs={12} md={6} lg={3}>
+        <ChartCard title={t("ETH" + EthPrice())}>
+          <VictoryChart height={config.chartHeight} width={config.chartWidth} theme={victoryTheme as any}>
+            <VictoryAxis crossAxis standalone={false} />
+            <VictoryAxis dependentAxis crossAxis domain={[1750, 2000]} standalone={false} />
+            <VictoryLine data={blocks.map(blockMapEthPrice)} />
           </VictoryChart>
         </ChartCard>
       </Grid>
